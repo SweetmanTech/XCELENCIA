f4c2b6b7ce941b080b622811c03bceed87ee058a
SeniorTech <tech.eng.me@gmail.com>
2024-02-06 12:05:31 -0500

    I complete top section ui

--- components/Pages/LandingPage/ArtworkAlbum.tsx
+++ components/Pages/LandingPage/ArtworkAlbum.tsx
@@ -1,8 +1,14 @@
-const ArtworkAlbum = () => {
-   return (
-    <>
-    </>
-   )
-}
+import FadeInWhenVisible from "@/components/FadeInWhenVisible"
+import Media from "@/shared/Media"
+
+const ArtworkAlbum = () => (
+  <FadeInWhenVisible>
+    <Media
+      link="/images/album.png"
+      blurLink="/images/album.png"
+      containerClasses="w-[325px] h-[326px]"
+    />
+  </FadeInWhenVisible>
+)
 
 export default ArtworkAlbum
--- components/Pages/LandingPage/MusicPlayer.tsx
+++ components/Pages/LandingPage/MusicPlayer.tsx
@@ -1,5 +1,14 @@
-const MusicPlayer = () => {
+import FadeInWhenVisible from "@/components/FadeInWhenVisible"
+import Media from "@/shared/Media"
 
-}
+const MusicPlayer = () => (
+  <FadeInWhenVisible>
+    <Media
+      link="/images/music.png"
+      blurLink="/images/music.png"
+      containerClasses="w-[330px] h-[326px]"
+    />
+  </FadeInWhenVisible>
+)
 
 export default MusicPlayer
