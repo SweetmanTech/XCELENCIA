3c61d46260f0007d0509db9b48075a73c8a2176d
SeniorTech <tech.eng.me@gmail.com>
2024-02-06 11:56:10 -0500

    I add layouts

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,22 @@
+import Content from "@/components/Layout/Content"
 import Layout from "../../Layout"
 import SeoHead from "../../SeoHead"
+import Media from "@/shared/Media"
 
 const LandingPage = () => (
   <Layout type="base">
     <SeoHead />
-    <div
-      className="xl:w-[1440px] lg:w-[1280px] md:w-[1024px] h-screen
-              flex items-center justify-around"
-    />
+    <Content>
+      <div className="w-full grid grid-cols-2">
+        <div className="col-span-2">
+          <Media
+            link="/images/video.png"
+            blurLink="/images/video.png"
+            containerClasses="w-[649px] h-[322px]"
+          />
+        </div>
+      </div>
+    </Content>
   </Layout>
 )
 
