bda0bee3fbc28706b6efbb4fbd343f36714733ff
SeniorTech <tech.eng.me@gmail.com>
2024-02-07 09:47:55 -0500

    I add rainbow modal if not signed

--- components/CollectAllButton/CollectAllButton.tsx
+++ components/CollectAllButton/CollectAllButton.tsx
@@ -1,11 +1,26 @@
-const CollectAllButton = () => (
-  <button
-    type="button"
-    className="text-[18px] font-bold uppercase bg-gray rounded-full
-        px-[20px] py-[10px]"
-  >
-    Collect All
-  </button>
-)
+import { useEthersSigner } from "@/hooks/useEthersSigner"
+import { useConnectModal } from "@rainbow-me/rainbowkit"
+
+const CollectAllButton = () => {
+  const signer = useEthersSigner()
+  const { openConnectModal } = useConnectModal()
+
+  const onClick = () => {
+    if (!signer) {
+      openConnectModal()
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className="text-[18px] font-bold uppercase bg-gray rounded-full
+          px-[20px] py-[10px]"
+      onClick={onClick}
+    >
+      Collect All
+    </button>
+  )
+}
 
 export default CollectAllButton
