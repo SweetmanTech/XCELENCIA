3b0b6a890174a82fa5ca2af412fa5f842ca2fd0d
SeniorTech <tech.eng.me@gmail.com>
2024-02-13 12:05:54 -0500

    I replace video as image

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -2,7 +2,7 @@ import Header from "../Header"
 import { ILayout } from "./types"
 
 const BaseLayout = ({ children }: ILayout) => (
-  <div className="w-full overflow-hidden bg-pink min-h-screen">
+  <div className="w-full overflow-hidden bg-pink min-h-screen flex justify-center">
     <Header />
     {children}
   </div>
