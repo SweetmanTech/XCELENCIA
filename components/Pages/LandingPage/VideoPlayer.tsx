4d26a1eb26eb623fbc2a825236c29192252a0f94
SeniorTech <tech.eng.me@gmail.com>
2024-02-13 12:05:54 -0500

    I replace video as image

--- components/Pages/LandingPage/VideoPlayer.tsx
+++ components/Pages/LandingPage/VideoPlayer.tsx
@@ -1,13 +1,8 @@
 import FadeInWhenVisible from "@/components/FadeInWhenVisible"
-import Media from "@/shared/Media"
 
 const VideoPlayer = () => (
   <FadeInWhenVisible>
-    <Media
-      link="/images/video.png"
-      blurLink="/images/video.png"
-      containerClasses="w-[649px] h-[322px]"
-    />
+    <video src="/hero.mp4" className="w-[649px] h-[322px]" muted loop autoPlay controls />
   </FadeInWhenVisible>
 )
 
