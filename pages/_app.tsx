de18ba16364ac27a23173b7f48b4bb36c7f4bf3a
SeniorTech <tech.eng.me@gmail.com>
2024-02-14 10:06:37 -0500

    I removed unused css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css"
 import "react-toastify/dist/ReactToastify.css"
-import "@rainbow-me/rainbowkit/styles.css"
 
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
