f2a95bf5315e993c59bf89231b51da817bf4400a
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-02-20 20:40:50 +0600

    player warpsound size issue fixed

--- components/Pages/LandingPage/MusicPlayer.tsx
+++ components/Pages/LandingPage/MusicPlayer.tsx
@@ -2,15 +2,10 @@ import FadeInWhenVisible from "@/components/FadeInWhenVisible"
 
 const MusicPlayer = () => (
   <FadeInWhenVisible>
-    {/* <Media
-      link="/images/music.png"
-      blurLink="/images/music.png"
-      containerClasses="w-[330px] h-[326px]"
-    /> */}
     <iframe
       src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://bafkreidsekbryzcxjdtwwvvjm7u6iligrkni3x3snqicwt452wz5dpadzi.ipfs.nftstorage.link/"
-      width="100%"
-      height="400"
+      width="326"
+      height="326"
       frameBorder="0"
       allow="autoplay"
       title="Xcelencia"
