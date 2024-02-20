2895c4486351129aa726907af9bb56eee78796c8
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-02-20 18:42:01 +0600

    player warpsound done

--- components/Pages/LandingPage/MusicPlayer.tsx
+++ components/Pages/LandingPage/MusicPlayer.tsx
@@ -1,12 +1,19 @@
 import FadeInWhenVisible from "@/components/FadeInWhenVisible"
-import Media from "@/shared/Media"
 
 const MusicPlayer = () => (
   <FadeInWhenVisible>
-    <Media
+    {/* <Media
       link="/images/music.png"
       blurLink="/images/music.png"
       containerClasses="w-[330px] h-[326px]"
+    /> */}
+    <iframe
+      src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://bafkreidsekbryzcxjdtwwvvjm7u6iligrkni3x3snqicwt452wz5dpadzi.ipfs.nftstorage.link/"
+      width="100%"
+      height="400"
+      frameBorder="0"
+      allow="autoplay"
+      title="Xcelencia"
     />
   </FadeInWhenVisible>
 )
