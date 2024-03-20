4249ce8678ec957ef320764863e16489c3537ebc
SeniorDev <tech.eng.me@gmail.com>
2024-03-20 11:27:25 -0400

    Revert "I update iframe"
    This reverts commit 04b5746a69bda2fd03bc8aa7943bb56b11c97fb1.

--- components/Pages/LandingPage/MusicPlayer.tsx
+++ components/Pages/LandingPage/MusicPlayer.tsx
@@ -3,7 +3,7 @@ import FadeInWhenVisible from "@/components/FadeInWhenVisible"
 const MusicPlayer = () => (
   <FadeInWhenVisible>
     <iframe
-      src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://ipfs.io/ipfs/QmPQsLmsGArK1n9TAfiyoJWdcN2hGGBBpTS7KASAipcAH7/playerlist.json"
+      src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://bafkreiegccagv2chicak5cg74d34dbghr4f3ykjzswmsuof45zmlizsp7y.ipfs.nftstorage.link/"
       width="326"
       height="326"
       frameBorder="0"
