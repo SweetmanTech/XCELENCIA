9a55adcab586de759683d8e03844867fef303cff
SeniorDev <tech.eng.me@gmail.com>
2024-03-21 19:49:17 -0400

    I add all external wallet

--- hooks/useConnectedWallet.tsx
+++ hooks/useConnectedWallet.tsx
@@ -4,10 +4,7 @@ import { useMemo } from "react"
 const useConnectedWallet = () => {
   const { wallets } = useWallets()
   const privyWallet = wallets?.find((wallet) => wallet.walletClientType === "privy")
-  const metamaskWallet = useMemo(
-    () => wallets?.find((wallet) => wallet.walletClientType === "metamask"),
-    [wallets],
-  )
+  const metamaskWallet = useMemo(() => wallets[1], [wallets])
   const connectedWallet = privyWallet?.address
 
   return { connectedWallet, privyWallet, metamaskWallet }
