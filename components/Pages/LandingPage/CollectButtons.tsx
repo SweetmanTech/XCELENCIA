91b6d2dab85b7d8a2db6558d3b45863314d6c8cf
SeniorDev <tech.eng.me@gmail.com>
2024-03-21 10:55:30 -0400

    I update home page ui

--- components/Pages/LandingPage/CollectButtons.tsx
+++ components/Pages/LandingPage/CollectButtons.tsx
@@ -1,5 +1,4 @@
 import CollectAllButton from "@/components/CollectAllButton"
-import CrossmintButton from "@/components/CrossmintButton"
 import FadeInWhenVisible from "@/components/FadeInWhenVisible"
 
 const CollectButtons = () => (
@@ -8,7 +7,6 @@ const CollectButtons = () => (
   gap-y-[15px] md:gap-y-0 md:gap-x-[10px]"
   >
     <CollectAllButton />
-    <CrossmintButton />
   </FadeInWhenVisible>
 )
 
