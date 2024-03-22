a27f9e06d52f7a0ca9455119c505ba0a25f8a939
SeniorDev <tech.eng.me@gmail.com>
2024-03-22 15:44:15 -0400

    I update link

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import Icon from "@/shared/Icon"
 const Footer = () => (
   <div className="flex w-full justify-center gap-[20px] py-[20px]">
     {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-    <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+    <a href="https://www.instagram.com/xcelencia" target="_blank" rel="noreferrer">
       <Icon name="instagram" color="white" raw size={40} />
     </a>
     {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-    <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+    <a href="https://twitter.com/xcelencia" target="_blank" rel="noreferrer">
       <Icon name="twitter" color="white" raw size={40} />
     </a>
   </div>
