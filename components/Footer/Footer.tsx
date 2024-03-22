09557ede99cc89fb620d83e21998c4bbded1362c
SeniorDev <tech.eng.me@gmail.com>
2024-03-22 15:26:42 -0400

    I fix eslint error

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
-/* eslint-disable */
 import Icon from "@/shared/Icon"
 
 const Footer = () => (
   <div className="flex w-full justify-center gap-[20px] py-[20px]">
+    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
     <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
       <Icon name="instagram" color="white" raw size={40} />
     </a>
+    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
     <a href="https://twitter.com/" target="_blank" rel="noreferrer">
       <Icon name="twitter" color="white" raw size={40} />
     </a>
