4e7b280b5292dd19ba416912ae03b537573d4b71
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-23 13:56:14 -0300

    I remove dev logging.

--- components/Pages/LandingPage/SoundCollectButton.tsx
+++ components/Pages/LandingPage/SoundCollectButton.tsx
@@ -3,31 +3,19 @@ import usePrivyWalletClient from "@/hooks/usePrivyWalletClient"
 import useTBAPurchase from "@/hooks/useTBAPurchase"
 import { getPublicClient } from "@/lib/clients"
 import { CHAIN, CHAIN_ID } from "@/lib/consts"
-import { Client } from "viem"
 
 const SoundCollectButton = ({ className = "" }) => {
   const { walletClient } = usePrivyWalletClient()
   const { externalWallet } = useConnectedWallet()
-  const { purchase, loading } = useTBAPurchase()
+  const { loading } = useTBAPurchase()
 
   const handleClick = async () => {
-    console.log("Sound SDK Collect", walletClient)
-    const [address] = await walletClient.getAddresses()
-    console.log("address", address)
     const SPOTIFY = "0xdE88522Dc78a6cc99C1CfF060d8BC744291E159a"
     const publicClient = getPublicClient(CHAIN_ID)
-    console.log("publicClient", publicClient)
-
-    const { isSoundEditionV1, isSoundEditionV2 } = await publicClient.soundEditionVersion({
-      contractAddress: SPOTIFY,
-    })
-    console.log("isSoundEditionV1", isSoundEditionV1)
-    console.log("isSoundEditionV2", isSoundEditionV2)
 
     const mintSchedules = await publicClient.editionV2.mintSchedules({
       editionAddress: SPOTIFY,
     })
-    console.log("mintSchedules", mintSchedules)
 
     const mintParams = await publicClient.editionV2.mintParameters({
       account: externalWallet.address,
@@ -37,19 +25,8 @@ const SoundCollectButton = ({ className = "" }) => {
       editionAddress: SPOTIFY,
       mintTo: externalWallet.address,
     })
-    console.log("mintParams", mintParams)
-    // return publicClient.editionV2.mintParameters({
-    //   account: walletClient.account,
-    //   chain: walletClient.walletClient.chain,
-    //   schedule: 0,
-    //   quantity: 1,
-    //   editionAddress: SPOTIFY,
-    //   mintTo: wallet.account.address,
-    // })
-    console.log("walletClient", walletClient)
-
     const hash = await walletClient.editionV2.mint(mintParams.mint)
-    console.log("hash", hash)
+    return hash
   }
 
   return (
