e13f6dcc0ee017c9cd1a698195a908f9d156620a
seniorTech <tech.eng.me@gmail.com>
2024-03-23 12:06:15 -0700

    I add fade effect

--- components/Pages/ImaginePage/ImaginePage.tsx
+++ components/Pages/ImaginePage/ImaginePage.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout"
 import Content from "@/components/Layout/Content"
 import SeoHead from "@/components/SeoHead"
+import FadeInWhenVisible from "@/components/FadeInWhenVisible"
 import SmartAlbum from "./SmartAlbum"
 import MusicPlayer from "../LandingPage/MusicPlayer"
 
@@ -9,13 +10,15 @@ const ImaginePage = () => (
     <SeoHead />
     <Content>
       <div className="w-full">
-        <div className="w-full text-white 
+        <FadeInWhenVisible
+          className="w-full text-white 
         lg:pl-[100px] 
         text-[40px] text-[white] 
         lg:text-left text-center
-        font-poppins_italic ">
+        font-poppins_italic "
+        >
           your smart album contains:
-        </div>
+        </FadeInWhenVisible>
         <div className="w-full grid grid-cols-1 lg:grid-cols-2 mt-[50px] gap-[50px]">
           <div className="flex justify-center lg:justify-start mt-[10px] lg:pl-[150px]">
             <MusicPlayer />
