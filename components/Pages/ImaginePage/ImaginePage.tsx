3d011da26bf08e05ed4d7ada6603434022b8e365
seniorTech <tech.eng.me@gmail.com>
2024-03-23 11:59:09 -0700

    I complete basic imagine page

--- components/Pages/ImaginePage/ImaginePage.tsx
+++ components/Pages/ImaginePage/ImaginePage.tsx
@@ -1,14 +1,25 @@
 import Layout from "@/components/Layout"
 import Content from "@/components/Layout/Content"
 import SeoHead from "@/components/SeoHead"
-import SignButton from "@/components/SignButton"
+import SmartAlbum from "./SmartAlbum"
+import MusicPlayer from "../LandingPage/MusicPlayer"
 
 const ImaginePage = () => (
   <Layout type="base">
     <SeoHead />
     <Content>
-      <div className="h-full flex justify-center items-center">
-        <SignButton />
+      <div className="w-full">
+        <div className="w-full text-white pl-[100px] text-[40px] text-[white] font-poppins_italic">
+          your smart album contains:
+        </div>
+        <div className="w-full grid grid-cols-2 mt-[50px]">
+          <div className="flex justify-start mt-[10px] pl-[150px]">
+            <MusicPlayer />
+          </div>
+          <div className="w-full flex justify-start">
+            <SmartAlbum />
+          </div>
+        </div>
       </div>
     </Content>
   </Layout>
