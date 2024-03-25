13b2c7535ab673645d2feb5d88dee04d01a6be77
seniorTech <tech.eng.me@gmail.com>
2024-03-25 11:46:53 -0400

    I refactor the code

---
+++ hooks/useCollectSound.tsx
@@ -0,0 +1,46 @@
+import { SOUNDXYZ_CHAIN, SPOTIFY_DROP_ADDRESS } from "@/lib/consts"
+import useConnectedWallet from "./useConnectedWallet"
+import usePrivyWalletClient from "./usePrivyWalletClient"
+import { getPublicClient } from "@/lib/clients"
+import { useState } from "react"
+
+const useSoundXYZCollect = () => {
+  const { walletClient } = usePrivyWalletClient()
+  const { externalWallet } = useConnectedWallet()
+  const [loading, setLoaindg] = useState(false)
+
+  const collectSoundXYZ = async () => {
+    const privyChainId = externalWallet.chainId
+    if (privyChainId !== `eip155:${SOUNDXYZ_CHAIN.id}`) {
+      await externalWallet.switchChain(SOUNDXYZ_CHAIN.id)
+      return
+    }
+    setLoaindg(true)
+    const publicClient = getPublicClient(SOUNDXYZ_CHAIN.id)
+    const anyPublicClient = publicClient as any
+
+    const mintSchedules = await anyPublicClient.editionV2.mintSchedules({
+      editionAddress: SPOTIFY_DROP_ADDRESS,
+    })
+
+    const mintParams = await anyPublicClient.editionV2.mintParameters({
+      account: externalWallet.address,
+      chain: SOUNDXYZ_CHAIN,
+      schedule: mintSchedules.activeSchedules[0],
+      quantity: 1,
+      editionAddress: SPOTIFY_DROP_ADDRESS,
+      mintTo: externalWallet.address,
+    })
+    const anyWallet = walletClient as any
+    const hash = await anyWallet.editionV2.mint(mintParams.mint)
+    setLoaindg(true)
+    return hash
+  }
+
+  return {
+    collectSoundXYZ,
+    loading,
+  }
+}
+
+export default useSoundXYZCollect
