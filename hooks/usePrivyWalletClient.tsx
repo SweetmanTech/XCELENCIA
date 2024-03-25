5be152f443f661220b5ae7d7214720b773aba671
seniorTech <tech.eng.me@gmail.com>
2024-03-25 14:41:56 -0400

    I built final codebase for the privy & wallet

--- hooks/usePrivyWalletClient.tsx
+++ hooks/usePrivyWalletClient.tsx
@@ -1,27 +1,26 @@
-import { CHAIN } from "@/lib/consts"
 import { useEffect, useState } from "react"
-import { Chain, WalletClient, createWalletClient, custom } from "viem"
+import { WalletClient, createWalletClient, custom } from "viem"
 import { editionV2WalletActionsMint } from "@soundxyz/sdk"
 import useConnectedWallet from "./useConnectedWallet"
 
-const usePrivyWalletClient = () => {
-  const { externalWallet } = useConnectedWallet()
+const usePrivyWalletClient = (chain) => {
+  const { connectedWallet, wallet } = useConnectedWallet()
   const [walletClient, setWalletClient] = useState<WalletClient | null>(null)
 
   useEffect(() => {
     const init = async () => {
-      const provider = await externalWallet.getEthereumProvider()
+      const provider = await wallet.getEthereumProvider()
       const response = createWalletClient({
-        chain: CHAIN as Chain,
-        account: externalWallet.address as `0x${string}`,
+        chain,
+        account: connectedWallet as `0x${string}`,
         transport: custom(provider),
       }).extend(editionV2WalletActionsMint)
       setWalletClient(response)
     }
 
-    if (!externalWallet) return
+    if (!connectedWallet || !chain) return
     init()
-  }, [externalWallet])
+  }, [connectedWallet, chain])
 
   return { walletClient }
 }
