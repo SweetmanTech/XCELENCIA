29a2f8d38f88b2b8f9d5081e2abdf82faa070c20
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-26 16:26:27 -0300

    I update hard-coded chainId in publicClient.

--- lib/tokenbound/client.tsx
+++ lib/tokenbound/client.tsx
@@ -5,5 +5,5 @@ import { CHAIN_ID } from "../consts"
 const publicClient = getPublicClient(CHAIN_ID)
 export const tokenboundPublicClient = new TokenboundClient({
   publicClient: publicClient as any,
-  chainId: 5,
+  chainId: CHAIN_ID,
 })
