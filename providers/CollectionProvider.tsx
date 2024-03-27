25d040fe47777be7cffc56c1e77c8b5c85cfff9e
SeniorDev <tech.eng.me@gmail.com>
2024-03-27 08:35:40 -0400

    I add collection provider to check balanceOf

---
+++ providers/CollectionProvider.tsx
@@ -0,0 +1,27 @@
+import useCheckCollected from "@/hooks/useCheckCollected"
+import { createContext, useMemo, useContext } from "react"
+
+const CollectionContext = createContext(null)
+
+const CollectionProvider = ({ children }) => {
+  const collectedData = useCheckCollected()
+
+  const value = useMemo(
+    () => ({
+        ...collectedData
+    }),
+    [collectedData],
+  )
+
+  return <CollectionContext.Provider value={value}>{children}</CollectionContext.Provider>
+}
+
+export const useCollectionProvider = () => {
+  const context = useContext(CollectionContext)
+  if (!context) {
+    throw new Error("useCollectionProvider must be used within a CollectionProvider")
+  }
+  return context
+}
+
+export default CollectionProvider
