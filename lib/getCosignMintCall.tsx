00d5d5f99a000aca7eda88270acf79d8e4da3da8
SeniorDev <tech.eng.me@gmail.com>
2024-03-28 16:17:46 -0400

    I remove async

--- lib/getCosignMintCall.tsx
+++ lib/getCosignMintCall.tsx
@@ -3,7 +3,7 @@ import cosignAbi from "@/lib/abi/catalog-cosign.json"
 import { BigNumber } from "ethers"
 import { CATALOGCOSIGN_ADDRESS, CATALOG_ID, CATALOG_PRICE, CATALOG_REFERRAL } from "./consts"
 
-const getCosignMintCall = async (mintRecipient) => {
+const getCosignMintCall = (mintRecipient) => {
   const cosignMintData = new Interface(cosignAbi).encodeFunctionData("purchaseTokenForRecipient", [
     BigNumber.from(CATALOG_ID).toString(),
     1,
