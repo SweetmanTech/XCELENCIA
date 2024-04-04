150cfb9bff5e6851409e6375d43855f53ddec31f
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-04 20:23:00 -0300

    I fix music player on imagine page.

--- components/Pages/LandingPage/MusicPlayer.tsx
+++ components/Pages/LandingPage/MusicPlayer.tsx
@@ -3,7 +3,7 @@ import FadeInWhenVisible from "@/components/FadeInWhenVisible"
 const MusicPlayer = () => (
   <FadeInWhenVisible>
     <iframe
-      src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://ipfs.io/ipfs/QmPQsLmsGArK1n9TAfiyoJWdcN2hGGBBpTS7KASAipcAH7/playerlist.json"
+      src="https://nftstorage.link/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://ipfs.io/ipfs/QmNj3eewckzHh3SkwnM3XDYRoFp7tzyjtQZLAjZLng6E81"
       width="326"
       height="326"
       frameBorder="0"
