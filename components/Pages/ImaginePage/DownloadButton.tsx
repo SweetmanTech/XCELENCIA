e2c94fcc94af57515fdf797eec45773bef5c9a8a
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-04 21:04:04 -0300

    I simpligy DownloadButton.

--- components/Pages/ImaginePage/DownloadButton.tsx
+++ components/Pages/ImaginePage/DownloadButton.tsx
@@ -1,26 +1,13 @@
 import { SEO_TITLE } from "@/lib/consts"
 
-const DownloadButton = ({ className = "" }) => {
-  const handleClick = () => {
-    const filePath = "https://ipfs.io/ipfs/QmToDp7wKCugm2HzA8zcujrFXgnKqsjuLNAYvn5vTaC9Pc"
-
-    const link = document.createElement("a")
-    link.href = filePath
-    link.download = "ene.zip"
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  }
-
-  return (
-    <a
-      href="https://ipfs.io/ipfs/QmToDp7wKCugm2HzA8zcujrFXgnKqsjuLNAYvn5vTaC9Pc"
-      className={`text-[18px] font-bold uppercase bg-gray rounded-full
+const DownloadButton = ({ className = "" }) => (
+  <a
+    href="https://ipfs.io/ipfs/QmToDp7wKCugm2HzA8zcujrFXgnKqsjuLNAYvn5vTaC9Pc"
+    className={`text-[18px] font-bold uppercase bg-gray rounded-full
 px-[20px] py-[10px] max-h-11 ${className}`}
-    >
-      Download {SEO_TITLE}
-    </a>
-  )
-}
+  >
+    Download {SEO_TITLE}
+  </a>
+)
 
 export default DownloadButton
