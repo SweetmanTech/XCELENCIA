c4384b2a8b6b85fc9d6b0dac9cc3b679fd906b4c
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-04 21:50:42 -0300

    I add price beneath  collect button.

--- components/Pages/LandingPage/CollectButtons.tsx
+++ components/Pages/LandingPage/CollectButtons.tsx
@@ -6,7 +6,10 @@ const CollectButtons = () => (
     className="w-full flex flex-col md:flex-row justify-center 
   gap-y-[15px] md:gap-y-0 md:gap-x-[10px]"
   >
-    <CollectAllButton />
+    <div className="flex flex-col justify-center items-center">
+      <CollectAllButton />
+      <div className="text-white">0.01269 ETH ($45)</div>
+    </div>
   </FadeInWhenVisible>
 )
 
