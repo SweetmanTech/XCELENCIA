a9d3fd47067fc217f69e87f634a7f0ed044ee48a
SeniorDev <tech.eng.me@gmail.com>
2024-04-17 09:31:31 -0400

    I complete spark page

--- components/Pages/StoryPage/StoryPage.tsx
+++ components/Pages/StoryPage/StoryPage.tsx
@@ -1,12 +1,14 @@
 import Layout from "@/components/Layout"
 import Journey from "./Journey"
 import Started from "./Started"
+import Spark from "./Spark"
 
 const StoryPage = () => (
   <Layout type="base">
     <div className="flex flex-col w-screen">
       <Journey />
       <Started />
+      <Spark />
     </div>
   </Layout>
 )
