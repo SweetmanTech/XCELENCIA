6db5636ec46fe15aa656bdadd7f58cba9eb57c8a
SeniorDev <tech.eng.me@gmail.com>
2024-04-18 11:07:47 -0400

    I add ethprice api

--- components/Pages/LandingPage/CollectButtons.tsx
+++ components/Pages/LandingPage/CollectButtons.tsx
@@ -1,16 +1,22 @@
 import CollectAllButton from "@/components/CollectAllButton"
 import FadeInWhenVisible from "@/components/FadeInWhenVisible"
+import useEthPrice from "@/hooks/useEthPrice"
+import getUsdConversion from "@/lib/getUSDConversion"
 
-const CollectButtons = () => (
-  <FadeInWhenVisible
-    className="w-full flex flex-col md:flex-row justify-center 
-  gap-y-[15px] md:gap-y-0 md:gap-x-[10px]"
-  >
-    <div className="flex flex-col justify-center items-center">
-      <CollectAllButton />
-      <div className="text-white">0.01269 ETH ($45)</div>
-    </div>
-  </FadeInWhenVisible>
-)
+const CollectButtons = () => {
+  const { ethPrice } = useEthPrice()
+
+  return (
+    <FadeInWhenVisible
+      className="w-full flex flex-col md:flex-row justify-center 
+    gap-y-[15px] md:gap-y-0 md:gap-x-[10px]"
+    >
+      <div className="flex flex-col justify-center items-center">
+        <CollectAllButton />
+        <div className="text-white">0.01269 ETH (${getUsdConversion(0.01269, ethPrice)})</div>
+      </div>
+    </FadeInWhenVisible>
+  )
+}
 
 export default CollectButtons
