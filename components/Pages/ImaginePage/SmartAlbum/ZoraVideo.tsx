31b8b730e0ff456dc1b2e7731b46a3fe6216e9d6
SeniorDev <tech.eng.me@gmail.com>
2024-04-19 05:03:20 -0400

    I update link

--- components/Pages/ImaginePage/SmartAlbum/ZoraVideo.tsx
+++ components/Pages/ImaginePage/SmartAlbum/ZoraVideo.tsx
@@ -1,7 +1,12 @@
 const ZoraVideo = () => (
   <div className="w-full aspect-[1/1] flex justify-center items-center">
     {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-    <video src="https://cloudflare-ipfs.com/ipfs/bafybeihpzem3f3bn46rfzrdhwwt6qosfsemkepsipuat67q5kdy46axn7q" width={444} className="aspect-video" controls />
+    <video
+      src="https://cloudflare-ipfs.com/ipfs/QmQ5rJgfLx6vsScm4FJo85SA2RGmCKHSCCGEqFJfoi4L9B/bafybeihpzem3f3bn46rfzrdhwwt6qosfsemkepsipuat67q5kdy46axn7q.mov"
+      width={444}
+      className="aspect-video"
+      controls
+    />
   </div>
 )
 
