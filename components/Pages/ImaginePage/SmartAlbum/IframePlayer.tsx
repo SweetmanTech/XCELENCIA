674eaf5d610c31afbdf0332729ab9756010c09ca
SeniorDev <tech.eng.me@gmail.com>
2024-05-01 12:33:09 -0400

    I update gate way

--- components/Pages/ImaginePage/SmartAlbum/IframePlayer.tsx
+++ components/Pages/ImaginePage/SmartAlbum/IframePlayer.tsx
@@ -1,6 +1,6 @@
 const IframePlayer = () => (
   <iframe
-    src="https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://ipfs.io/ipfs/QmQ2qwc89nZkKYdq9LT1L483eeUBvUpjtFnBFyUi1VYNSb/playlist"
+    src="https://cloudflare-ipfs.com/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=https://cloudflare-ipfs.com/ipfs/QmQ2qwc89nZkKYdq9LT1L483eeUBvUpjtFnBFyUi1VYNSb/playlist"
     width="444"
     height="444"
     frameBorder="0"
