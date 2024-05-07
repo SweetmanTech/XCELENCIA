3606814e220b5b486fc01f52e6ee1174ec34b69c
TarunNakrani <tarunnakrani90@gmail.com>
2024-05-07 17:30:26 +0530

    change collage img name and alt

--- components/Pages/ImaginePage/SmartAlbum/ZoraImage.tsx
+++ components/Pages/ImaginePage/SmartAlbum/ZoraImage.tsx
@@ -1,7 +1,7 @@
 const ZoraImage = () => (
   <div className="w-full aspect-[1/1] flex justify-center items-center">
     {/* eslint-disable-next-line jsx-a11y/media-has-caption, @next/next/no-img-element */}
-    <img src="images/Collage-JPG.jpg" alt="Collage JPG" />
+    <img src="images/collage-notes.jpg" alt="collage notes" />
   </div>
 )
 
